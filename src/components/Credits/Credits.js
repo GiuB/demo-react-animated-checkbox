c390cb50263c4952eaf5a1618e1b1863a2c46526
GiuB <web@giub.it>
2020-11-02 12:00:32 +0100

    mirno update

--- src/components/Credits/Credits.js
+++ src/components/Credits/Credits.js
@@ -12,7 +12,7 @@ import Me from '../../assets/img/me.jpeg'
 export default function Credits(props) {
   return (
     <C.CreditsWrapper>
-      <h1>Demo React Swiper Carousel with 3d effect</h1>
+      <h1>Demo React Swiper Carousel with 3d hover effect</h1>
       <sub style={{ color: 'red', fontStyle: 'italic' }}>
         Give it a try with a desktop >= 640px
       </sub>
